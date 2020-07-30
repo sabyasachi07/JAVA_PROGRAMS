15f7da5c3f9274b5a383260dcfc9b12e9fda0cfc
sabyasachi07 <sahoosabyasachi07@gmail.com>
2020-07-31 01:25:17 +0530

    rectified the  error

--- assingment2/complex2.java
+++ assingment2/complex2.java
@@ -1,35 +1,44 @@
 import java.util.*;
 
-class Complex2{
+class Complex2 {
 
-        int a,b;
+    float a, b;
 
-    Complex2(int num)
-    {
-        Scanner sc=new Scanner(System.in);
+    Complex2(int num) {
+        Scanner scan = new Scanner(System.in);
+
         System.out.println("---|numbers is in the form a+bi|---");
 
-        System.out.println("Enter the value of a(real)"+num+":");
-        a=sc.nextInt();
-        System.out.println("Enter the value of b(imaginary)"+num+":");
-        b=sc.nextInt();
-     	
+        System.out.println("Enter the value of a(real)" + num + ":");
+
+        if (scan.hasNextFloat()) {
+            a = scan.nextFloat();
+
+        }
+        System.out.println("Enter the value of b(imag.)" + num + ":");
+        if (scan.hasNextFloat()) {
+            b = scan.nextFloat();
+
+        }
+
     }
-    Complex2(Complex2 obj1,Complex2 obj2)
-    {
-        a=obj1.a+obj2.a;
-        b=obj1.b+obj2.b;
+
+    Complex2(Complex2 obj1, Complex2 obj2) {
+        a = obj1.a + obj2.a;
+        b = obj1.b + obj2.b;
     }
-    void display()
-    {
-        System.out.println("The sum is:"+a+"+i"+b);
+
+    void display() {
+        System.out.println("The sum is:" + a + "+i" + b);
     }
-    public static void main(String[] args)
-    {   
-        Complex2 obj1=new Complex2(1);
-        Complex2 obj2=new Complex2(2);
-        Complex2 obj3=new Complex2(obj1,obj2);
+
+    public static void main(String[] args) {
+        Complex2 obj1 = new Complex2(1);
+
+        Complex2 obj2 = new Complex2(2);
+
+        Complex2 obj3 = new Complex2(obj1, obj2);
         obj3.display();
-    
+
     }
 }
